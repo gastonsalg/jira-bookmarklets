09a2d55fdbca5a7aa56ee801aa81bce6f79c4e68
Gastón Salgueiro <gaston.salgueiro@xing.com>
2014-10-01 10:26:38 +0200

    Fixes setting options for the addTicket script

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
       addPluginFooter: function (layout) {
         var options = layout ? ', { layoutName: ' + layout + ' }' : '';
         return '' +
-          'var xingJiraApp = new xing.jira.Application("", ' + options + ');' +
+          'var xingJiraApp = new xing.jira.Application(""' + options + ');' +
           'xingJiraApp.versionTimestamp="<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>";' +
           'xingJiraApp.version="<%= pkg.version %>";' +
           'xingJiraApp.cacheTicketHandler();' +
